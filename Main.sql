6c84e03a0addcb1378e958ee3085b30c07231e6c
Frederik Steenberg <75301197+Fevek05@users.noreply.github.com>
2023-10-24 10:06:24 +0200

    Update Main.sql

--- Main.sql
+++ Main.sql
@@ -1,5 +1,5 @@
-CREATE TABLE `mandag24`.`teams` (`Id` SERIAL NOT NULL , `Name` TEXT NOT NULL , PRIMARY KEY (`Id`)) ENGINE = InnoDB;
+CREATE TABLE `mandag24`.`teams` (`id` SERIAL NOT NULL , `name` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 
-CREATE TABLE `mandag24`.`student` (`Id` SERIAL NOT NULL , `Name` TEXT NOT NULL , `TeamsId` BIGINT NOT NULL , PRIMARY KEY (`Id`)) ENGINE = InnoDB;
+CREATE TABLE `mandag24`.`student` (`id` SERIAL NOT NULL , `name` TEXT NOT NULL , `teams_id` BIGINT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 
 ALTER TABLE `mandag24`.`student` ADD  FOREIGN KEY (`teams_id`) REFERENCES `teams`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
